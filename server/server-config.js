991f8510c6addb7311a3b14aaa06b1e700606bc9
Placid Rodrigues <placidrod@gmail.com>
2017-07-15 19:46:32 -0700

    (feat) Compose ejs templates

--- server/server-config.js
+++ server/server-config.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const partials = require('express-partials');
 const app = express();
 
+app.set('views', path.join(__dirname, '/../views'));
+app.set('view engine', 'ejs');
+app.use(partials());
+
 app.use(express.static(path.join(__dirname, '/../public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
 module.exports = app;
