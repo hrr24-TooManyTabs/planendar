395b3812644b29cc39acd2ecdcf6bff910053b39
Placid Rodrigues <placidrod@gmail.com>
2017-07-15 20:12:14 -0700

    (fix) errors due to non-checking of existing tables

--- db/config.js
+++ db/config.js
@@ -10,38 +10,51 @@ var knex = require('knex')({
   useNullAsDefault: true
 })
 
-var database = require('Bookshelf')(knex);
+var db = require('Bookshelf')(knex);
 
-database.knex.schema.createTable('users', function(users) {
-  users.increments('id').primary();
-  users.string('email', 254);
-  users.string('password', 254);
-}).then((table) => {
-  console.log("users table exists, ", table);
-})
+db.knex.schema.hasTable('users').then(function(exists) {
+  if(!exists) {
+    db.knex.schema.createTable('users', function(users) {
+      users.increments('id').primary();
+      users.string('email', 254);
+      users.string('password', 254);
+    }).then((table) => {
+      console.log("Created table ", table);
+    });
+  }
+});
 
-database.knex.schema.createTable('appointments', function(appointments) {
-  appointments.increments('id').primary();
-  appointments.integer('user_id');
-  appointments.foreign('user_id').references('users.id');
-  appointments.string('title', 140);
-  appointments.string('description', 300);
-  appointments.date('start_date');
-  appointments.dateTime('start_date_time');
-  appointments.date('end_date');
-  appointments.dateTime('end_date_time');
-}).then((table) => {
-  console.log("appointments table exists, ", table);
-})
+db.knex.schema.hasTable('appointments').then(function(exists) {
+  if(!exists) {
+    db.knex.schema.createTable('appointments', function(appointments) {
+      appointments.increments('id').primary();
+      appointments.integer('user_id');
+      appointments.foreign('user_id').references('users.id');
+      appointments.string('title', 140);
+      appointments.string('description', 300);
+      appointments.date('start_date');
+      appointments.dateTime('start_date_time');
+      appointments.date('end_date');
+      appointments.dateTime('end_date_time');
+    }).then((table) => {
+      console.log("Created table ", table);
+    });
+  }
+});
 
-database.knex.schema.createTable('reminders', function(reminders) {
-  reminders.increments('id').primary();
-  reminders.integer('appointment_id');
-  reminders.foreign('appointment_id').references('appointments.id');
-  reminders.string('description', 300);
-  reminders.integer('minutes');
-}).then((table) => {
-  console.log("reminders table exists, ", table);
-})
+db.knex.schema.hasTable('reminders').then(function(exists) {
+  if(!exists) {
+    db.knex.schema.createTable('reminders', function(reminders) {
+      reminders.increments('id').primary();
+      reminders.integer('appointment_id');
+      reminders.foreign('appointment_id').references('appointments.id');
+      reminders.string('description', 300);
+      reminders.integer('minutes');
+    }).then((table) => {
+      console.log("reminders table exists, ", table);
+    });
+  }
+});
 
-module.exports = database;
+
+module.exports = db;
