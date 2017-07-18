0987c47e16402138a55c8018f0af38eb55f5b921
aajungroth <aajungroth@ucdavis.edu>
2017-07-17 17:08:23 -0700

    Wrote create appointment, need to test

--- public/components/CreateAppointment.jsx
+++ public/components/CreateAppointment.jsx
@@ -1,24 +1,45 @@
 import React from 'react';
 
-const CreateAppointment = ({reminders, createReminder, deleteReminder, createAppointment}) => {
-
+const CreateAppointment = ({reminders, reminderInput, appointementInput, createReminder, deleteReminder, createAppointment, updateReminder, updateAppointment}) => {
 
   const handleCreateReminder = (e) => {
     console.log('handleCreateReminder');
     console.log('e', e);
+    e.preventDefault();
     createReminder();
   };
 
   const handleDeleteReminder = (e) => {
     console.log('handleDeleteReminder');
     console.log('e', e);
-    deleteReminder();
+    e.preventDefault();
+    deleteReminder(e.target.key);
   };
 
   const handleCreateAppointment = (e) => {
     console.log('handleCreateAppointment');
     console.log('e', e);
-    createAppointment();
+    e.preventDefault();
+    if ((appointmentInput.title !== '') &&
+      (appointmentInput.start_date !== '') &&
+      (appointmentInput.start_date_time !== '') &&
+      (appointmentInput.end_date_time !== '')) {
+      createAppointment();
+    } else {
+      console.error('require fields have not been filled out');
+    }
+  };
+
+  const handleReminderChange = (e) => {
+    console.log('handleReminderChange');
+    console.log('e', e);
+    updateReminder(e.target.name, e.target.value);
+  };
+
+  const handleAppointmentChange = (e) => {
+    console.log('handleAppointmentChange');
+    console.log('e', e);
+    updateAppointment(e.target.name, e.target.value);
   };
 
   return (
@@ -38,7 +59,7 @@ const CreateAppointment = ({reminders, createReminder, deleteReminder, createApp
         <button type="submit" className="btn btn-default" onClick={handleCreateAppointment}>Create Appointment</button>
 
         <div className="form-group">
-          <input type="text" className="form-control" placeholder="Name"/>
+          <input type="text" className="form-control" placeholder="Name" name="title" value={appointment.title}/>
         </div>
 
         <label>&ensp;Start</label>
@@ -50,24 +71,28 @@ const CreateAppointment = ({reminders, createReminder, deleteReminder, createApp
         */}
 
         <div className="form-group">
-          <input type="date" className="form-control" placeholder="Start Date"/>
+          <input type="date" className="form-control" placeholder="Start Date" name="start_date" value={appointment.start_date}/>
         </div>
 
         <div className="form-group">
-          <input type="time" className="form-control" placeholder="Start Time"/>
+          <input type="time" className="form-control" placeholder="Start Time" name="start_date_time" value={appointment.start_date_time}/>
         </div>
 
         <label>&ensp;Finish</label>
         <div className="form-group">
-          <input type="date" className="form-control" placeholder="End Date"/>
+          <input type="date" className="form-control" placeholder="End Date" name="end_date" value={appointment.end_date}/>
         </div>
 
         <div className="form-group">
-          <input type="time" className="form-control" placeholder="End Time"/>
+          <input type="time" className="form-control" placeholder="End Time" name="end_date_time" value={appointment.end_date_time}/>
         </div>
 
         <div className="form-group">
-          <textarea className="form-control" placeholder="Description" rows="1" cols="148"></textarea>
+          <textarea className="form-control" placeholder="Description" name="description" value={appointment.description} rows="1" cols="148" resize="none"></textarea>
+        </div>
+
+        <div className="form-group">
+          <textarea className="form-control" placeholder="Location" name="location" value={appointment.location} rows="1" cols="148" resize="none"></textarea>
         </div>
 
       </form>
@@ -95,7 +120,7 @@ const CreateAppointment = ({reminders, createReminder, deleteReminder, createApp
         <button type="submit" className="btn btn-default" onClick={handleCreateReminder}>Add Reminder</button>
 
         <div className="form-group">
-          <input type="number" min="0" max="60" step="5" className="form-control" placeholder="m"/>
+          <input type="number" min="0" max="60" step="5" className="form-control" placeholder="m" name="minutes" value={appointment.minutes}/>
         </div>
       </form>
     </div>
