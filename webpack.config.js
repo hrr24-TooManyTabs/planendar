b34863d6381a6fd05e8e016aad9f2c0d7f839606
aajungroth <aajungroth@ucdavis.edu>
2017-07-18 22:48:37 -0700

    Changed ./public/index.js back to ./index.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
   context: path.resolve(__dirname, './public'),
-  entry: './public/index.js',
+  entry: './index.js',
   output: {
     path: path.resolve(__dirname, './public/dist'),
     filename: 'bundle.js'
