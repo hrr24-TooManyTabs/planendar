341ac9b9bf5d7dfb2a7f66856f10eea3346ba277
aajungroth <aajungroth@ucdavis.edu>
2017-07-18 21:54:32 -0700

    Set up the front end testing with karma, mocha, and webpack, wrote some basic tests

---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+var webpackConfig = require('./webpack.config.js');
+webpackConfig.entry = function(){return {}};
+
+module.exports = function (config) {
+  config.set({
+    browsers: [ 'Chrome' ], //run in Chrome
+    singleRun: true, //just run once by default
+    frameworks: [ 'mocha' ], //use the mocha test framework
+    files: [
+      './tests.webpack.js' //just load this file
+    ],
+    preprocessors: {
+      './public/dist/bundle.js': ['webpack'],
+      './tests.webpack.js': [ 'webpack', 'sourcemap' ] //preprocess with webpack and our sourcemap loader
+      //adding ,'babel' causes warnings, but we might want to add it back in to test in
+    },
+    reporters: [ 'mocha', 'coverage' ], //report results in this format
+    webpack: webpackConfig,
+    webpackServer: {
+      noInfo: true //please don't spam the console when running in karma!
+    },
+    coverageReporter: {
+      type: 'html', //produces a html document after code is run
+      dir: 'coverage/' //path to created html doc
+    }
+  });
+};
