6c9cb8c175ef35b4cefe139f715898f71d2cdd67
aajungroth <aajungroth@ucdavis.edu>
2017-07-20 16:39:28 -0700

    Collapsed input fields to two lines, removed extra comments, commented functions

--- public/components/Navbar.jsx
+++ public/components/Navbar.jsx
@@ -19,32 +19,8 @@ const Navbar = ({reminders, reminderInput, appointmentInput, createReminder, del
 
         {/*<!-- Collect the nav links, forms, and other content for toggling -->*/}
         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        {/*
-          <ul className="nav navbar-nav">
-            <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
-            <li><a href="#">Link</a></li>
-            <li className="dropdown">
-              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-              <ul className="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" className="divider"></li>
-                <li><a href="#">Separated link</a></li>
-                <li role="separator" className="divider"></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-        */}
-        {/*
-          <form className="navbar-form navbar-left">
-            <div className="form-group">
-              <input type="text" className="form-control" placeholder="Search"/>
-            </div>
-            <button type="submit" className="btn btn-default">Submit</button>
-          </form>
-        */}
+
+          {/*<!-- The create appointment input form -->*/}
           <CreateAppointment
           reminders={reminders}
           reminderInput={reminderInput}
@@ -54,23 +30,9 @@ const Navbar = ({reminders, reminderInput, appointmentInput, createReminder, del
           createAppointment={createAppointment}
           updateReminder={updateReminder}
           updateAppointment={updateAppointment}></CreateAppointment>
-        {/*
-          <ul className="nav navbar-nav navbar-right">
-            <li><a href="#">Link</a></li>
-            <li className="dropdown">
-              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-              <ul className="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" className="divider"></li>
-                <li><a href="#">Separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-        */}
+
         </div>{/*<!-- /.navbar-collapse -->*/}
-      </div>{/*<!-- /.container-fluid -->*/}
+      </div>{/*<!-- /.container -->*/}
     </nav>
   );
 };
