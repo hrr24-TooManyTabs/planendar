636201fb46b85edec284774ea636f690dfb96509
aajungroth <aajungroth@ucdavis.edu>
2017-07-21 17:33:15 -0700

    Added an example api key file

---
+++ public/apixu.example.js
@@ -0,0 +1,16 @@
+//The apixu key will go in apixu.js
+//window.APIXU_API_KEY = "THE_API_KEY_HERE";
+
+/*
+Current Weather
+HTTP: http://api.apixu.com/v1/current.json?key=THE_API_KEY_HERE&q=Paris
+
+HTTPS: https://api.apixu.com/v1/current.json?key=THE_API_KEY_HERE&q=Paris
+*/
+
+/*
+Forecast Weather
+HTTP: http://api.apixu.com/v1/forecast.json?key=THE_API_KEY_HERE&q=Paris
+
+HTTPS: https://api.apixu.com/v1/forecast.json?key=THE_API_KEY_HERE&q=Paris
+*/
