95e646f62fa0b7d47e373a7a932c3f07dbad521e
aajungroth <aajungroth@ucdavis.edu>
2017-07-26 15:14:23 -0700

    Added css styling for pop ups

--- public/components/App.jsx
+++ public/components/App.jsx
@@ -41,6 +41,25 @@ export default class App extends React.Component {
     this.deleteEvent = this.deleteEvent.bind(this);
     this.getWeather = this.getWeather.bind(this);
 
+    Popup.registerPlugin('popover', function (content, target) {
+        this.create({
+            content: content,
+            className: 'popover',
+            noOverlay: true,
+            position: function (box) {
+                let bodyRect      = document.body.getBoundingClientRect();
+                let btnRect       = target.getBoundingClientRect();
+                let btnOffsetTop  = btnRect.top - bodyRect.top;
+                let btnOffsetLeft = btnRect.left - bodyRect.left;
+                let scroll        = document.documentElement.scrollTop || document.body.scrollTop;
+
+                box.style.top  = (btnOffsetTop - box.offsetHeight - 10) - scroll + 'px';
+                box.style.left = (btnOffsetLeft + (target.offsetWidth / 2) - (box.offsetWidth / 2)) + 'px';
+                box.style.margin = 0;
+                box.style.opacity = 1;
+            }
+        });
+    });
   }
 
   //Adds a reminder to the newReminders array in state
@@ -303,6 +322,8 @@ export default class App extends React.Component {
           right: ['ok']
       }
     });
+
+    Popup.plugins().popover('This popup will be displayed right above this button.', document.body);
   }
 
   getWeather(selectedCity) {
